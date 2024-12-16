a4dea1d0d275003b181b97f586912ca8f3b71ed2
RajaeRs <rajaerasezine@gmail.com>
2024-12-16 12:02:49 +0100

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Welcome from '@/components/layout/Welcome';
 import About from '@/components/layout/About';
-import Project from '@/components/layout/Project';
-import Contact from '@/components/layout/Contact';
+// import Project from '@/components/layout/Project';
+// import Contact from '@/components/layout/Contact';
 
 export default function Home() {
   return (
